5bd83de76f9b57b196644a5e4ffd4a63104b0945
urooj haider <suhz1100@gmail.com>
2025-05-31 15:52:07 +0500

    add

--- java.js
+++ java.js
@@ -10,16 +10,36 @@
 //         img.src = "applegreen.png"
 //     })
 
-    
+    // ================ SINGLE BUTTON =============================
+
+// let img = document.getElementById("img");
+
+
+//     document.getElementById("btn").addEventListener('click', function(){
+//        if( img.src.includes('applered.png')) {
+//         img.src = "applegreen.png";
+//        } else{
+//         img.src = "applered.png";
+//     } 
+//     }
+// )
+// =================== CODE END ===========================================
+
+//========================== SLIDER START =============================== 
+
 let img = document.getElementById("img");
 
+document.getElementById("btn1").addEventListener('click', function(){
+    img.src = "slider1.jpg";
+});
+document.getElementById("btn2").addEventListener('click', function(){
+    img.src = "slider2.jpg";
+});
+document.getElementById("btn3").addEventListener('click', function(){
+    img.src = "slider3.jpg";
+});
+document.getElementById("btn4").addEventListener('click', function(){
+    img.src = "slider4.jpg";
+});
 
-    document.getElementById("btn").addEventListener('click', function(){
-       if( img.src.includes('applered.png')) {
-        img.src = "applegreen.png";
-       } else{
-        img.src = "applered.png";
-    } 
-    }
-)
-
+   
